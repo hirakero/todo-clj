f22eea47e669073109a0cae1295702d0f7302c27
kero_hirake <k16.h.white@gmail.com>
2021-08-23 02:15:57 +0900

    part 3 自前ルーティング完了

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -4,10 +4,10 @@
 
 (defonce server (atom nil))
 
-(defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "<h1>Hello, world!?</h1>"})
+;; (defn handler [req]
+;;   {:status 200
+;;    :headers {"Content-Type" "text/html"}
+;;    :body "<h1>Hello, world!?</h1>"})
 
 (defn start-server []
   (when-not @server
@@ -23,4 +23,56 @@
     (stop-server)
     (start-server)))
 
+(defn ok [body]
+  {:status 200
+   :body body})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found</h1>"})
+
+(defn home-view [req] ;home-body
+  "<h1>home</h1>
+   <a href=\"/todo\"todo list</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+(def todo-list
+  [{:title "breakfast"}
+   {:title "garbage removal"}
+   {:title "buy eggs"}
+   {:title "bath wash"}])
+
+(defn todo-index-view [req]
+  `("<h1>todo list</h1>"
+    "<ul>"
+    ~@(for [{:keys [title]} todo-list]
+        (str "<li>" title "</li>"))
+    "</ul>"))
+
+(defn todo-index [req]
+  (-> (todo-index-view req)
+      ok
+      html))
+
+(def routes
+  {"/" home
+   "/todo" todo-index})
+
+
+(defn match-route [uri]
+  (get routes uri))
+
+(defn handler [req]
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
 
