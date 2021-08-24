74a8d4ac2e8598038336c9640ca7dfa853704a11
kero_hirake <k16.h.white@gmail.com>
2021-08-24 12:48:49 +0900

    part 3 機能ごとにファイルを分割

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,17 +1,21 @@
 
 (ns todo-clj.core
-  (:require [ring.adapter.jetty :as server]))
+  (:require [compojure.core :refer [routes]]
+            [ring.adapter.jetty :as server]
+            [todo-clj.handler.main :refer [main-routes]]
+            [todo-clj.handler.todo :refer [todo-routes]]))
 
 (defonce server (atom nil))
 
-;; (defn handler [req]
-;;   {:status 200
-;;    :headers {"Content-Type" "text/html"}
-;;    :body "<h1>Hello, world!?</h1>"})
+(def app
+  (routes
+   todo-routes
+   main-routes))
+
 
 (defn start-server []
   (when-not @server
-    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
+    (reset! server (server/run-jetty #'app {:port 3000 :join? false}))))
 
 (defn stop-server []
   (when @server
@@ -22,57 +26,3 @@
   (when @server
     (stop-server)
     (start-server)))
-
-(defn ok [body]
-  {:status 200
-   :body body})
-
-(defn html [res]
-  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
-
-(defn not-found []
-  {:status 404
-   :body "<h1>404 page not found</h1>"})
-
-(defn home-view [req] ;home-body
-  "<h1>home</h1>
-   <a href=\"/todo\"todo list</a>")
-
-(defn home [req]
-  (-> (home-view req)
-      ok
-      html))
-
-(def todo-list
-  [{:title "breakfast"}
-   {:title "garbage removal"}
-   {:title "buy eggs"}
-   {:title "bath wash"}])
-
-(defn todo-index-view [req]
-  `("<h1>todo list</h1>"
-    "<ul>"
-    ~@(for [{:keys [title]} todo-list]
-        (str "<li>" title "</li>"))
-    "</ul>"))
-
-(defn todo-index [req]
-  (-> (todo-index-view req)
-      ok
-      html))
-
-(def routes
-  {"/" home
-   "/todo" todo-index})
-
-
-(defn match-route [uri]
-  (get routes uri))
-
-(defn handler [req]
-  (let [uri (:uri req)
-        maybe-fn (match-route uri)]
-    (if maybe-fn
-      (maybe-fn req)
-      (not-found))))
-
---
+++ src/todo_clj/handler/main.clj
@@ -0,0 +1,19 @@
+(ns todo-clj.handler.main
+  (:require [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
+            [todo-clj.util.response :as res]))
+
+
+(defn home-view [req] ;home-body
+  "<h1>home!</h1>
+   <a href=\"/todo\"todo list</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      res/response
+      res/html))
+
+(defroutes main-routes
+  (GET "/" _ home)
+  (route/not-found "<h1> 404 page not found</h1>"))
+
